7183f15fa5939aaa08d5a935c9cc09f4fdf8d658
Matt32609 <vthematt@gmail.com>
2026-02-17 23:54:08 +0800

    Add files via upload

--- MUET actual score.py
+++ MUET actual score.py
@@ -8,12 +8,17 @@ MUET_result()
 def answer():
         selection = input("Please select an option out of the four . Please respond with either 'listening' , 'speaking' , 'reading' or 'writing' to continue. ").lower()
         if selection == "listening":
-            score = input("Please enter the obtained score that you received as written on the certificate :")
-            result = (float(score) / 90) * 30
-            if result.is_integer:
-                print(f"Your actual score : {int(result)}/30")
-            else:
-                print(f"Your actual score : {result:2f}/30")
+            score_input = input("Please enter the obtained score that you received as written on the certificate :")
+            try:
+                score = float(score_input)
+                result = (score / 90) * 30
+                if result.is_integer:
+                    print(f"Your actual score : {int(result)}/30")
+                else:
+                    print(f"Your actual score : {result:2f}/30") 
+            except ValueError:
+                print("Invalid response.")
+                return True          
             confirmation = input("Do you wish to continue? Please respond with 'yes' or 'no' to respond.")
             if confirmation == "yes":
                 print(f"Alright")
@@ -21,12 +26,17 @@ def answer():
             else:
                 exit()
         elif selection == "speaking":
-            score = input("Please enter the obtained score that you received as written on the certificate :").lower()
-            result = (float(score) / 90) * 42
-            if result.is_integer:
-                print(f"Your actual score : {int(result)}/42")
-            else:
-                print(f"Your actual score : {result:2f}/42")
+            score_input = input("Please enter the obtained score that you received as written on the certificate :")
+            try:
+                score = float(score_input)
+                result = (score / 90) * 42
+                if result.is_integer:
+                    print(f"Your actual score : {int(result)}/42")
+                else:
+                    print(f"Your actual score : {result:2f}/42") 
+            except ValueError:
+                print("Invalid response.")
+                return True          
             confirmation = input("Do you wish to continue? Please respond with 'yes' or 'no' to respond.")
             if confirmation == "yes":
                 print(f"Alright")
@@ -34,12 +44,17 @@ def answer():
             else:
                 exit()
         elif selection == "reading":
-            score = input("Please enter the obtained score that you received as written on the certificate :").lower()
-            result = (float(score) / 90) * 40
-            if result.is_integer:
-                print(f"Your actual score : {int(result)}/40")
-            else:
-                print(f"Your actual score : {result:2f}/40")
+            score_input = input("Please enter the obtained score that you received as written on the certificate :")
+            try:
+                score = float(score_input)
+                result = (score / 90) * 40
+                if result.is_integer:
+                    print(f"Your actual score : {int(result)}/40")
+                else:
+                    print(f"Your actual score : {result:2f}/40") 
+            except ValueError:
+                print("Invalid response.")
+                return True          
             confirmation = input("Do you wish to continue? Please respond with 'yes' or 'no' to respond.")
             if confirmation == "yes":
                 print(f"Alright")
